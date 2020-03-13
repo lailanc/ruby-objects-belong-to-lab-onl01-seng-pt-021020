945c02959b533973daa14b8fe2fe0933a2b500e8
Laila Campbell <lailacampbell96@gmail.com>
2020-03-13 04:54:19 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,8 +1,9 @@
-class Artist 
-  attr_accessor :name 
+class Artist
+ 
+  attr_accessor :artist
   def initialize(artist)
     @artist = artist
-  end 
-end 
-  beyonce = Artist.new 
-  beyonce.artist 
+  end
+end
+beyonce = Artist.new("Beyonce")
+beyonce.artist 
